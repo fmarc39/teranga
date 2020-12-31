5d0051069ee35943aa9dfdf63409dcc4db746dc2
fmarc39 <73823754+fmarc39@users.noreply.github.com>
2020-12-31 01:25:21 +0100

    début de la mise en place de la gallery photo grid

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
-// Mise en place du menu reactif 
+// CREATION DE LA SECTION QUI VA PERMETTRE D'OBERSVER LA SCROLL DE LA PAGE ET DE FAIRE APPARAITRE LES EFFETS 
 
+const ratio = 0.1
+
+const options = {
+
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+  var callback = function(entries, observer) { 
+    entries.forEach(entry => {
+
+        if (entry.intersectionRatio > ratio) {
+
+            entry.target.classList.add('reveal-visible')           
+        }
+    })
+  }
+  var observer = new IntersectionObserver(callback, options);
+
+  var target = document.querySelectorAll("[class*=reveal-").forEach(function (r) {
+    observer.observe(r)
+  })
+
+  observer.observe(target);
