a4b6bd7ec132f64fb0c05c802a16eba9c919eaee
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-10-09 21:11:55 +0200

    slider test

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,6 @@ const app = {
     app.topUpBtn = document.getElementById("up-arrow");
 
     // Initialisation des fonctions
-    app.mainTitleAnnimation();
     app.viewPortMobile();
     app.handlePopUpEffect();
 
@@ -35,22 +34,6 @@ const app = {
 
   handleScrollBtn: function () {
     window.scroll(0, innerHeight);
-  },
-
-  mainTitleAnnimation: function () {
-    let textWrapper = document.querySelector(".ml9 .letters");
-    textWrapper.innerHTML = textWrapper.textContent.replace(
-      /\S/g,
-      "<span class='letter'>$&</span>"
-    );
-
-    anime.timeline({ loop: false }).add({
-      targets: ".ml9 .letter",
-      scale: [0, 1],
-      duration: 2000,
-      elasticity: 600,
-      delay: (el, i) => 45 * (i + 1),
-    });
   },
 
   handleTopUpBtn: function () {
